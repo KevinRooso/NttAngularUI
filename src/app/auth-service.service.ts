352864a939d90bfb3c19dd1dbddee393db24963d
ayushisingh <ayushi.singh@bellurbis.com>
2020-04-10 19:47:48 +0530

    changes in approval flow

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -6,8 +6,8 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class AuthServiceService {
- // private url = "http://localhost:8080/";
- private url = "https://ntt-backend-app.herokuapp.com/";
+  private url = "http://localhost:8080/";
+ //private url = "https://ntt-backend-app.herokuapp.com/";
  public headers = new HttpHeaders({
   Authorization : 'Bearer ' + localStorage.getItem("token")
 });
@@ -166,5 +166,12 @@ export class AuthServiceService {
 
   getAllHomeData():Observable<any>{
     return this.http.get<any>(this.url+'api/public/homePage', {headers:this.headers})
+  }
+
+  savePublish(id,flag):Observable<any>{
+    return this.http.put<any>(this.url+'api/admin/event/publish/'+id+'/'+flag,null, {headers:this.headers});
+  }
+  saveActive(id,flag):Observable<any>{
+    return this.http.put<any>(this.url+'api/admin/event/active/'+id+'/'+flag,null, {headers:this.headers});
   }
 }
