cfc0d5e97e68a7f31ae32e2ad729913ddf0cc789
unknown <dheeraj.kishore@bellurbis.com>
2020-04-11 21:48:58 +0530

    Event Agenda

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -19,6 +19,7 @@ export class CreateEventComponent implements OnInit {
 
   createEventForm: FormGroup;
   addTagForm: FormGroup;
+  addAgenda: FormGroup;
 
   allData: any[] = [];
   tagsList: string[] = [];
@@ -40,6 +41,7 @@ export class CreateEventComponent implements OnInit {
   today = new Date();
   newtoday = new Date();
   closingDate = new Date();
+  endingDate = new Date();
   regStartDate = new Date();
   regEndDate = new Date();
 
@@ -49,6 +51,9 @@ export class CreateEventComponent implements OnInit {
   userList: any[] = [];
 
   @ViewChild('closeModel', { static: true }) closeModel;
+  @ViewChild('closeModelAgenda', { static: true }) closeModelAgenda;
+  @ViewChild('agendaUpdate', { static: true }) agendaUpdate;
+
 
   isEvent: boolean = false;
   isWebinar: boolean = false;
@@ -58,6 +63,11 @@ export class CreateEventComponent implements OnInit {
 
   imageValid: boolean = false;
   imageValid2: boolean = false;
+
+  agendaData:any[] = [];
+
+
+  // endingDate: any;
 
 
 
@@ -115,6 +125,16 @@ export class CreateEventComponent implements OnInit {
     this.addTagForm = formBuilder.group({
       name: ['', Validators.required],
       keywords: ['', Validators.required],
+    })
+    this.addAgenda = formBuilder.group({
+      title: [''],
+      topic: [''],
+      startDate: [''],
+      endDate: [''],
+      speakerList: [''],
+      isBreak:[''],
+      idData:['-1'],
+      id:['0']
     })
     console.log("validation chcek=",this.createEventForm.controls['thumbnailImageUrl'].valid);
   }
@@ -193,9 +213,6 @@ export class CreateEventComponent implements OnInit {
         console.log("Image", this.articleImage);
         this.imageValid = false;
         this.snackBar.open('Image successfully uploaded', 'Close', {duration: 5000});
-        // this.createEventForm.controls['thumbnailImageUrl'].setValidators(null);
-        // this.createEventForm.controls['thumbnailImageUrl'].updateValueAndValidity();
-        //alert('SUCCESS !!');
       })
   }
   uploadAttachment() {
@@ -208,7 +225,6 @@ export class CreateEventComponent implements OnInit {
         console.log("File", this.attachFile);
         this.imageValid2 = false;
         this.snackBar.open('Image successfully uploaded', 'Close', {duration: 5000});
-        //alert('SUCCESS !!');
       })
   }
 
@@ -318,7 +334,8 @@ export class CreateEventComponent implements OnInit {
         "detailImageUrl": this.attachFile,
         "categoryTypeId": this.createEventForm.controls['categoryTypeId'].value,
         "tagList": tags,
-        "eventSchedule": schedule,
+       // "eventSchedule": schedule,
+        "eventSchedule": this.agendaData,
         "targetUserType": this.createEventForm.controls['targetUserType'].value,
         "autoApproveParticipant": false,
         "isbreak": false,
@@ -336,17 +353,17 @@ export class CreateEventComponent implements OnInit {
 
       console.log("Post Data", objData);
 
-      // this.authService.saveEventDetails(objData).subscribe(
-      //   (response) => {
-      //     this.snackBar.open('Event successfully created', 'Close', {duration: 5000});
+      this.authService.saveEventDetails(objData).subscribe(
+        (response) => {
+          this.snackBar.open('Event successfully created', 'Close', {duration: 5000});
 
-      //     this.submitted = false;
-      //     this.router.navigate(['events']);
-      //   },
-      //   (error) => {
-      //     this.snackBar.open(error, 'Close');
-      //    }
-      // )
+          this.submitted = false;
+          this.router.navigate(['events']);
+        },
+        (error) => {
+          this.snackBar.open(error, 'Close');
+         }
+      )
      }
     else {
       this.snackBar.open('Please fill all mandatory fields', 'Close', {duration: 5000});
@@ -354,7 +371,49 @@ export class CreateEventComponent implements OnInit {
 
   }
 
-  createTag() {
+  createAgenda(){
+    // if (this.addAgenda.valid) {
+      console.log("Check",this.addAgenda.controls['idData'].value);
+    let obj= {
+      "title": this.addAgenda.controls['title'].value,
+      "topic": this.addAgenda.controls['topic'].value,
+      "isBreak": this.addAgenda.controls['isBreak'].value,
+      "endDate": this.addAgenda.controls['endDate'].value,
+      "startDate": this.addAgenda.controls['startDate'].value,
+      "speakerList": this.addAgenda.controls['speakerList'].value,
+      "id":0,
+      "idData":-1
+    }
+    if(this.addAgenda.value['idData']!= -1){
+      obj['idData'] = this.addAgenda.value['idData'];
+    }else{
+      obj['idData'] = -1;
+    }
+    if(obj.idData == -1){
+      this.agendaData.push(obj);
+    }else{
+      this.agendaData[(obj.idData)]=obj;
+    }
+
+    console.log("agenda", obj);
+    console.log("updaate data", this.addAgenda.value);
+    this.addAgenda.reset()
+    this.closeModelAgenda.nativeElement.click();
+  }
+// }
+  delete(i){
+    this.agendaData.splice(i,1);
+  }
+  updateAgenda(i){
+    // alert(i);
+    this.agendaData[i].idData = i;
+    console.log("log", this.agendaData[i]);
+    this.addAgenda.setValue(this.agendaData[i]);
+    this.agendaUpdate.nativeElement.click();
+
+    //this.agendaData[i] = this.addAgenda
+  }
+    createTag() {
     if (this.addTagForm.valid) {
       let flag = true;
       this.tagData.forEach(m => {
@@ -390,33 +449,14 @@ export class CreateEventComponent implements OnInit {
   Back() {
     this.location.back(); // <-- go back to previous location on cancel
   }
-
-  // fileProgress1(fileInput: any) {
-  //   this.fileData = <File>fileInput.target.files[0];
-  //   this.preview1();
-  // }
-  // preview1() {
-  //   // Show preview
-  //   var mimeType = this.fileData.type;
-  //   if (mimeType.match(/image\/*/) == null) {
-  //     return;
-  //   }
-
-  //   var reader = new FileReader();
-  //   reader.readAsDataURL(this.fileData);
-  //   reader.onload = (_event) => {
-  //     this.previewUrl2 = reader.result;
-  //   }
-  // }
-  // getNewLocation(){
-  //   this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl(this.createEventForm.get(['locationMapUrl']).value);
-  //   this.mapUrl = this.createEventForm.get(['locationMapUrl']).value;
-  //  console.log("locationMapUrl", this.locationURL);
-  // }
   maxCDate() {
     console.log("Closing Date", this.createEventForm.get(['startDate']).value);
     this.closingDate = this.createEventForm.get(['startDate']).value;
    this.regStartDate = this.closingDate;
+  }
+  maxEDate() {
+    console.log("ending Date", this.createEventForm.get(['endDate']).value);
+    this.endingDate = this.createEventForm.get(['endDate']).value;
   }
   maxRegDate() {
     this.regEndDate = this.createEventForm.get(['registrationStartDate']).value;
