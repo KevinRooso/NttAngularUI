507af61f20e3223c9a6059497fe9aeb230671fc6
dheerajkraaj <dheeraj.kishore@bellurbis.com>
2020-04-27 14:34:36 +0530

    styling css lint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
   ...defaultEnvironment,
   API_ENDPOINT: 'https://ntt-be-dev-app.herokuapp.com/',
   NODE_ENV: 'local',
-  PROD: 'false',
-  LOG: 'true',
+  PROD: false,
+  LOG: true,
   LOG_LEVEL: 'debug',
-  ROLLBAR_ACCESS_TOKEN: '6edfc016b1664223a2264b9d080206a1',
-  ROLLBAR_ENABLE: 'true',
+  ROLLBAR_ACCESS_TOKEN: '',
+  ROLLBAR_ENABLE: false,
 };
