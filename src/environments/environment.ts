b6d6887c83eb18fd3300517ec51185c0c0679083
hemantsingh <hemant.singh@bellurbis.com>
2020-05-08 16:51:23 +0530

    new changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,7 @@ import {environment as defaultEnvironment} from "./environment.default";
 
   export const environment = {
     ...defaultEnvironment,
-    API_ENDPOINT: 'http://localhost:8080/',
-   // API_ENDPOINT: 'https://ntt-be-dev-app.herokuapp.com/',
+    API_ENDPOINT: 'https://ntt-be-dev-app.herokuapp.com/',
     NODE_ENV: 'local',
     PROD: false,
     LOG: true,
