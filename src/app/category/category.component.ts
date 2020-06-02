be789d5a0911a1996fac9e22b749888ac3966393
dheerajkraaj <dheeraj.kishore@bellurbis.com>
2020-06-02 19:20:56 +0530

    import as excel

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { AuthServiceService } from '../auth-service.service';
 import { MatTableDataSource } from '@angular/material/table';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -16,6 +16,7 @@ export class CategoryComponent implements OnInit {
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild('TABLE') table: ElementRef;
 
   constructor(private authService: AuthServiceService) {}
 
@@ -31,8 +32,6 @@ export class CategoryComponent implements OnInit {
         };
         this.categoryData.push(obj);
       });
-      // this.dataSource = new MatTableDataSource(this.categoryData);
-      // const catArr = res.body;
       this.dataSource = new MatTableDataSource(this.categoryData);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
@@ -45,5 +44,15 @@ export class CategoryComponent implements OnInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  exportAsExcel() {
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement); // converts a DOM TABLE element to a worksheet
+    // const ws: XLSX.WorkSheet= XLSX.utils.json_to_sheet(this.dataSource.data);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'Sheet.xlsx');
   }
 }
