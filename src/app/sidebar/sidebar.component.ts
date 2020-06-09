917061b2cac0d53d63911d7568703561eaf640f4
dheerajkraaj <dheeraj.kishore@bellurbis.com>
2020-06-09 20:42:44 +0530

    usertype belluser only show full sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,16 +9,21 @@ import { AuthServiceService } from '../auth-service.service';
 export class SidebarComponent implements OnInit {
   userRoles: any;
   flag = false;
+  userType: any;
   constructor(private service: AuthServiceService) {}
 
   ngOnInit(): void {
     this.service.getUserDetail().subscribe((res) => {
       this.userRoles = res.roles;
+      this.userType = res.userType;
       this.userRoles.forEach((i) => {
         if (i.name === 'ADMIN_EMPLOYEE_USER') {
           this.flag = true;
         }
       });
+      if (this.userType.id === 10) {
+        this.flag = true;
+      }
     });
   }
 }
