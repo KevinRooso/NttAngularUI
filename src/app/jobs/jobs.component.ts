3ff466075324516687316b5fb0a2995cbbe77728
Yash Rikhi <yash.rikhi@bellurbis.com>
2020-06-30 18:18:16 +0530

    Jobs Ui and Api implemented

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -4,6 +4,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { AuthServiceService } from '../auth-service.service';
 import cronstrue from 'cronstrue';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-jobs',
@@ -11,35 +12,57 @@ import cronstrue from 'cronstrue';
   styleUrls: ['./jobs.component.css'],
 })
 export class JobsComponent implements OnInit {
-  displayedColumns: string[] = ['seq', 'name', 'schedule', 'action'];
+  displayedColumns: string[] = ['seq', 'displayName', 'schedule', 'action'];
   dataSource: any;
+  jobId: any;
+  jobName: any;
   jobsData: any[] = [];
+  schedules = [
+    {
+      name: cronstrue.toString('0 */1 * ? * *'),
+      cron: '0 */1 * ? * *',
+    },
+    {
+      name: cronstrue.toString('0 * * * *'),
+      cron: '0 * * * *',
+    },
+    {
+      name: cronstrue.toString('0 0 0 * * ?'),
+      cron: '0 0 0 * * ?',
+    },
+    {
+      name: cronstrue.toString('0 0 * * 6,0'),
+      cron: '0 0 * * 6,0',
+    },
+    {
+      name: cronstrue.toString('0 0 * * 1-5'),
+      cron: '0 0 * * 1-5',
+    },
+    {
+      name: cronstrue.toString('0 0 1 * *'),
+      cron: '0 0 1 * *',
+    },
+    {
+      name: cronstrue.toString('0 0 31 3 *'),
+      cron: '0 0 31 3 *',
+    },
+  ];
   show = false;
+  chosenSchedule: any;
+  editCronForm: any;
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild('closeModel', { static: true }) closeModel;
 
-  constructor(private service: AuthServiceService) {}
+  constructor(private service: AuthServiceService, private frmbuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.show = true;
-    this.service.getJobsList().subscribe((res) => {
-      this.show = false;
-      res.body.forEach((element, index) => {
-        element.schedulerName = element.schedulerName.replace(/_/g, ' ');
-        const displayName = element.schedulerName.charAt(0).toUpperCase() + element.schedulerName.slice(1);
-        const obj = {
-          id: element.id,
-          seq: index + 1,
-          name: displayName,
-          schedule: cronstrue.toString(element.scheduleTime),
-        };
-        this.jobsData.push(obj);
-      });
-      this.dataSource = new MatTableDataSource(this.jobsData);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+    this.editCronForm = this.frmbuilder.group({
+      cron: ['', Validators.required],
     });
+    this.getJobsData();
   }
 
   applyFilter(event: Event) {
@@ -49,5 +72,46 @@ export class JobsComponent implements OnInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  getJobsData() {
+    this.jobsData = [];
+    this.service.getJobsList().subscribe((res) => {
+      res.body.forEach((element, index) => {
+        let dName = element.schedulerName.replace(/_/g, ' ');
+        dName = dName.charAt(0).toUpperCase() + dName.slice(1);
+        const obj = {
+          id: element.id,
+          seq: index + 1,
+          name: element.schedulerName,
+          displayName: dName,
+          schedule: cronstrue.toString(element.scheduleTime),
+        };
+        this.jobsData.push(obj);
+      });
+      this.dataSource = new MatTableDataSource(this.jobsData);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.show = false;
+    });
+  }
+
+  getJobId(id: any, name: any) {
+    this.jobId = id;
+    this.jobName = name;
+  }
+
+  changeSchedule() {
+    const obj = {
+      id: this.jobId,
+      scheduleTime: this.editCronForm.controls['cron'].value,
+      schedulerName: this.jobName,
+    };
+
+    this.service.editCronJob(obj).subscribe((_res) => {
+      this.show = true;
+      this.closeModel.nativeElement.click();
+      this.getJobsData();
+    });
   }
 }
