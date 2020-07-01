ad4a53738f3f5375cd43037ea62c1f7242a57bac
Yash Rikhi <yash.rikhi@bellurbis.com>
2020-07-01 14:19:15 +0530

    Home Config Loading Fixed

--- src/app/home-Configuration/banner-ui/banner-ui.component.ts
+++ src/app/home-Configuration/banner-ui/banner-ui.component.ts
@@ -5,23 +5,19 @@ import { AuthServiceService } from 'src/app/auth-service.service';
 @Component({
   selector: 'app-banner-ui',
   templateUrl: './banner-ui.component.html',
-  styleUrls: ['./banner-ui.component.css']
+  styleUrls: ['./banner-ui.component.css'],
 })
 export class BannerUiComponent implements OnInit {
-
-  constructor(private service:AuthServiceService,
-    private formBuilder:FormBuilder) {
-
-   }
+  constructor(private service: AuthServiceService, private formBuilder: FormBuilder) {}
   bannerConfigurationForm: FormGroup;
   bannerBlock;
   bannerSelectedValue;
-  selectBlockData:any[]=[];
+  selectBlockData: any[] = [];
   bannerSequence;
-  eventData:any[]=[];
-  show=false;
-  pFlag=false;
-  cFlag=false;
+  eventData: any[] = [];
+  show = false;
+  pFlag = false;
+  cFlag = false;
   // tslint:disable-next-line: no-input-rename
   @Input('userType') userType: string;
   // tslint:disable-next-line: no-input-rename
@@ -50,10 +46,9 @@ export class BannerUiComponent implements OnInit {
       datafieldType: ['', Validators.required],
       dataFieldId: ['', Validators.required],
     });
-    if(this.bannerData){
+    if (this.bannerData) {
       this.setBannerData();
-      }
-
+    }
   }
   // tslint:disable-next-line:use-lifecycle-interface
   ngOnChanges() {
@@ -64,12 +59,12 @@ export class BannerUiComponent implements OnInit {
       this.pFlag = false;
       this.cFlag = true;
     }
-    if(this.bannerData){
-    this.setBannerData();
+    if (this.bannerData) {
+      this.setBannerData();
     }
   }
-  setBannerData(){
-    this.show=true;
+  setBannerData() {
+    this.show = true;
     this.bannerBlock = this.bannerData.type;
     this.bannerSelectedValue = this.bannerData.id;
     if (this.bannerConfigurationForm) {
@@ -78,25 +73,25 @@ export class BannerUiComponent implements OnInit {
     }
     const durl = this.blocks.find((x) => x.apiName === this.bannerBlock).url.split('?')[1];
 
-    this.service.getBannerBlockDetail(durl,this.pFlag,this.cFlag).subscribe((res) => {
+    this.service.getBannerBlockDetail(durl, this.pFlag, this.cFlag).subscribe((res) => {
       this.selectBlockData = res.body;
       this.show = false;
     });
   }
-  emitBanner(){
-    const obj=this.bannerConfigurationForm.value;
-    obj.sequenceNumber=this.sequence;
-    if(this.bannerConfigurationForm.valid){
-      this.bannerToEmit.emit(obj)
+  emitBanner() {
+    const obj = this.bannerConfigurationForm.value;
+    obj.sequenceNumber = this.sequence;
+    if (this.bannerConfigurationForm.valid) {
+      this.bannerToEmit.emit(obj);
     }
   }
   getSelectedBlockData(value) {
-   // const value1 = this.blocks.find((x) => x.apiName === value).url.split('?')[0];
+    // const value1 = this.blocks.find((x) => x.apiName === value).url.split('?')[0];
     value = this.blocks.find((x) => x.apiName === value).url.split('?')[1];
     this.show = true;
-    this.service.getBannerBlockDetail(value,this.pFlag,this.cFlag).subscribe(
+    this.service.getBannerBlockDetail(value, this.pFlag, this.cFlag).subscribe(
       (res) => {
-       this.selectBlockData=res.body;
+        this.selectBlockData = res.body;
         this.show = false;
       },
       (_error) => {
@@ -104,8 +99,7 @@ export class BannerUiComponent implements OnInit {
       }
     );
   }
-  removeForm(){
+  removeForm() {
     this.removeBanner.emit(this.sequence);
   }
-
 }
